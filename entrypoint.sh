2cb65cb3943faccd8468f64cdfdffeb659803cf0
Emmanuel <kipronofb@outlook.com>
2024-07-24 14:16:10 +0300

    hive conf options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,32 +3,6 @@
 set -x
 
 : ${DB_DRIVER:=derby}
-: ${POSTGRES_HOST:=localhost}
-: ${POSTGRES_PORT:=5432}
-: ${POSTGRES_USER:=hive}
-: ${POSTGRES_PASSWORD:=hive}
-: ${POSTGRES_DB:=metastore}
-: ${METASTORE_URI:=thrift://localhost:9083}
-: ${WAREHOUSE_LOCATION:=/user/hive/warehouse}
-: ${SERVICE_NAME:=metastore}
-: ${AWS_ACCESS_KEY_ID:=}
-: ${AWS_SECRET_ACCESS_KEY:=}
-
-echo "DB_DRIVER: $DB_DRIVER"
-
-export DB_DRIVER=$DB_DRIVER
-export POSTGRES_HOST=$POSTGRES_HOST
-export POSTGRES_PORT=$POSTGRES_PORT
-export POSTGRES_USER=$POSTGRES_USER
-export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-export POSTGRES_DB=$POSTGRES_DB
-export METASTORE_URI=$METASTORE_URI
-export WAREHOUSE_LOCATION=$WAREHOUSE_LOCATION
-export SERVICE_NAME=$SERVICE_NAME
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-
-
 
 SKIP_SCHEMA_INIT="${IS_RESUME:-false}"
 
@@ -82,15 +56,11 @@ fi
 exec $HIVE_HOME/bin/hive \
         --skiphadoopversion \
         --skiphbasecp \
-        --service $SERVICE_NAME \
+        --service $SERVICE_NAME\
         --hiveconf fs.s3a.access.key=$AWS_ACCESS_KEY_ID \
         --hiveconf fs.s3a.secret.key=$AWS_SECRET_ACCESS_KEY \
         --hiveconf hive.metastore.warehouse.dir=$WAREHOUSE_LOCATION \
         --hiveconf hive.metastore.uris=$METASTORE_URI \
-        --hiveconf fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider \
-        --hiveconf hive.root.logger=DEBUG,console \
-        --hiveconf fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
-        --hiveconf javax.jdo.option.ConnectionDriverName=org.postgresql.Driver \
-        --hiveconf javax.jdo.option.ConnectionUserName=$POSTGRES_USER \
-        --hiveconf javax.jdo.option.ConnectionPassword=$POSTGRES_PASSWORD \
-        --hiveconf javax.jdo.option.ConnectionURL=jdbc:$DB_DRIVER://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
+        --hiveconf fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider\
+        --hiveconf hive.root.logger=DEBUG,console\
+        --hiveconf fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem
