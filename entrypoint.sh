4f49ce0ccaa51d5f8ad3b81669812ad412221180
Emmanuel <kipronofb@outlook.com>
2024-07-24 14:27:45 +0300

    hive conf options

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,5 +62,4 @@ exec $HIVE_HOME/bin/hive \
         --hiveconf hive.metastore.warehouse.dir=$WAREHOUSE_LOCATION \
         --hiveconf hive.metastore.uris=$METASTORE_URI \
         --hiveconf fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider\
-        --hiveconf hive.root.logger=DEBUG,console\
         --hiveconf fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem
