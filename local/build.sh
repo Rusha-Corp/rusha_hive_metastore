a4394d5ed1a74adcbc35975c4df082f5f0baef23
Emmanuel <emmanuel@amband.co.ke>
2025-01-27 13:15:47 +0100

    update: account number from env

--- local/build.sh
+++ local/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-docker buildx build \
-    --platform linux/amd64 \
-    -t ${AWS_ACCOUNT_ID}.dkr.ecr.eu-west-2.amazonaws.com/dev-hive-metastore:v1 \
-    . --push
-
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.region.amazonaws.com
+export $(grep -v '^#' .env | xargs)
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 docker buildx build \
     --platform linux/amd64 \
